bf3ccdf3cd99e0d8f25316f8baca8dfc8f08cc7d
JulianGarzon22 <bryangarzon22@gmail.com>
2019-09-28 00:53:15 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+      title: "",
+      message: ""
+    };
+  }
+
+  handleForm = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  submit = e => {
+    e.preventDefault();
+    this.setState(state => {
+      const messages = state.messages.concat({
+        title: this.state.title,
+        message: this.state.message
+      });
+
+      return {
+        messages,
+        title: "",
+        message: ""
+      };
+    });
+    console.log("mensaje enviado");
+    console.log(this.state.messages);
+  };
+
+  render() {
+    return (
+      <div className="container">
+        {this.state.messages &&
+          this.state.messages.map(m => {
+            return (
+              <div key={m.title}>
+                <h2>{m.title}</h2>
+                <h4>{m.message}</h4>
+              </div>
+            );
+          })}
+        <form onSubmit={this.submit}>
+          <label htmlFor="title">Título</label>
+          <input
+            autoComplete="off"
+            placeholder="Escribe un Título"
+            type="text"
+            name="title"
+            value={this.state.title}
+            onChange={this.handleForm}
+          />
+          <label htmlFor="message">Mensaje</label>
+          <input
+            autoComplete="off"
+            placeholder="Escribe un mensaje"
+            type="text"
+            name="message"
+            value={this.state.message}
+            onChange={this.handleForm}
+          />
+          <input type="submit" />
+        </form>
+        <br />
+        {this.state.title}
+        <br />
+        {this.state.message}
+      </div>
+    );
+  }
 }
 
 export default App;
